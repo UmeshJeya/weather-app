e6c06f74de513f86b40fc1e8a8394bb8b3e1688a
Umesh Jeya <umeshk.jeyathevan@gmail.com>
2023-02-01 14:39:59 -0500

    first push for weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+// React Modules
+import { useState, useEffect } from 'react';
+// Components
+
+// Assets/Styling
 import './App.css';
 
-function App() {
+// TO ADD IN A USEEFFECT
+// PERFORM A NETWORK
+// I WANT TO GRAB MY CITY FROM A SUBMIT EVENT
+
+const App = () => {
+  const [city, setCity] = useState("");
+  const [queryCity, setQueryCity] = useState("");
+  const [temperature, setTemperature] = useState("");
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const controlledInput = city.trim();
+    setQueryCity(controlledInput);
+  }
+
+  useEffect(() => {
+    const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+    url.search = new URLSearchParams({
+      q: queryCity,
+      appid: "b0ac25c4be51ba28bb37aa4ad7e64a33",
+      units: "metric"
+    })
+
+    if (queryCity !== "") {
+      fetch(url)
+        .then(data => data.json())
+        .then(response => setTemperature(response.main.temp))
+    }
+
+
+
+
+  }, [queryCity])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <h1>Weather App!</h1>
+
+      <form onSubmit={handleFormSubmit}>
+        <label htmlFor="city">Enter a city:</label>
+        <input
+          type="text"
+          id="city"
+          onChange={handleInputChange}
+        />
+        <button onClick={handleFormSubmit}>Submit</button>
+      </form>
+
+      {
+        temperature ? (
+          <div>
+            <p>{temperature}</p>
+          </div>
+        ) : null
+      }
+    </main>
+  )
 }
 
 export default App;
